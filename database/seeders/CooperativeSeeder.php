eb5557df5128ca0bbe6285d153364094e4f8a709
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:52:47 +0200

    database/seeders/CooperativeSeeder:

--- database/seeders/CooperativeSeeder.php
+++ database/seeders/CooperativeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cooperative;
 use Illuminate\Database\Seeder;
 
 class CooperativeSeeder extends Seeder
@@ -13,6 +14,6 @@ class CooperativeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Cooperative::factory()->count(10)->create();
     }
 }
