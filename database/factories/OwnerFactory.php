219a0d74eee929718fe5ce6ef4fcd7f53b9517ba
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:46:37 +0200

    database/factories/OwnerFactory:

--- database/factories/OwnerFactory.php
+++ database/factories/OwnerFactory.php
@@ -2,19 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Owner;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OwnerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Owner::class;
+
     public function definition()
     {
         return [
-            //
+            'names' => $this->faker->name,
+            'telephone' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'),
+            'status' => 'pending',
         ];
     }
 }
