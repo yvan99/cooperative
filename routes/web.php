979e267b98dcdf66756241ceacef186948879605
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 15:18:38 +0200

    routes/web:

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaffAuthController;
+use App\Http\Controllers\AuditorAuthController;
+use App\Http\Controllers\OwnerAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('staff')->group(function () {
+    Route::get('/login', [StaffAuthController::class, 'showLoginForm'])->name('staff.login');
+    Route::post('/login', [StaffAuthController::class, 'login']);
+    Route::get('/register', [StaffAuthController::class, 'showRegistrationForm'])->name('staff.register');
+    Route::post('/register', [StaffAuthController::class, 'register']);
+    Route::post('/logout', [StaffAuthController::class, 'logout'])->name('staff.logout');
+});
+
+// Auditor routes
+Route::prefix('auditor')->group(function () {
+    Route::get('/login', [AuditorAuthController::class, 'showLoginForm'])->name('auditor.login');
+    Route::post('/login', [AuditorAuthController::class, 'login']);
+    Route::get('/register', [AuditorAuthController::class, 'showRegistrationForm'])->name('auditor.register');
+    Route::post('/register', [AuditorAuthController::class, 'register']);
+    Route::post('/logout', [AuditorAuthController::class, 'logout'])->name('auditor.logout');
+});
+
+// Owner routes
+Route::prefix('owner')->group(function () {
+    Route::get('/login', [OwnerAuthController::class, 'showLoginForm'])->name('owner.login');
+    Route::post('/login', [OwnerAuthController::class, 'login']);
+    Route::get('/register', [OwnerAuthController::class, 'showRegistrationForm'])->name('owner.register');
+    Route::post('/register', [OwnerAuthController::class, 'register']);
+    Route::post('/logout', [OwnerAuthController::class, 'logout'])->name('owner.logout');
 });
