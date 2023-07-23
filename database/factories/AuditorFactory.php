d7d51d3ad702f9a9402aeadde13245b98cac5531
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:45:56 +0200

    database/factories/AuditorFactory:

--- database/factories/AuditorFactory.php
+++ database/factories/AuditorFactory.php
@@ -2,19 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Auditor;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AuditorFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Auditor::class;
+
     public function definition()
     {
         return [
-            //
+            'names' => $this->faker->name,
+            'telephone' => $this->faker->phoneNumber,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'),
         ];
     }
 }
