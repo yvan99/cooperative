193c006f540ee00356dbb7c06d08ef67c20f88bf
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 14:49:50 +0200

    app/Http/Controllers:

--- app/Http/Controllers/OwnerAuthController.php
+++ app/Http/Controllers/OwnerAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AuditorController extends Controller
+class OwnerAuthController extends Controller
 {
     //
 }
--- app/Http/Controllers/StaffAuthController.php
+++ app/Http/Controllers/StaffAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OwnerController extends Controller
+class StaffAuthController extends Controller
 {
     //
 }
