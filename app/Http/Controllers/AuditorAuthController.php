3696db6d4bcd538746365b72b1e73a191ee8dfd3
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 15:01:39 +0200

    app/Http/Controllers:

--- app/Http/Controllers/AuditorAuthController.php
+++ app/Http/Controllers/AuditorAuthController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Auditor;
+use App\Http\Requests\AuditorRegisterRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuditorAuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('auditor.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('auditor')->attempt($credentials)) {
+            return redirect()->intended('/auditor/dashboard');
+        }
+
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('auditor.register');
+    }
+
+    public function register(AuditorRegisterRequest $request)
+    {
+        $validatedData = $request->validated();
+        $randomPassword = bin2hex(random_bytes(6));
+
+        $auditor = Auditor::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'telephone' => $validatedData['telephone'],
+            'password' => bcrypt($randomPassword),
+        ]);
+
+
+        $smsApi = new SmsApiController();
+        $message = "Dear RCA Auditor" . $validatedData['name'] . " , your registration is successful. Your password is: $randomPassword";
+        $smsApi->sendSms($auditor->telephone, $message);
+
+        Auth::guard('auditor')->login($auditor);
+
+        return redirect('/auditor/dashboard');
+    }
+
+    public function logout()
+    {
+        Auth::guard('auditor')->logout();
+        return redirect('/auditor/login');
+    }
 }
--- app/Http/Controllers/OwnerAuthController.php
+++ app/Http/Controllers/OwnerAuthController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Owner;
+use App\Http\Requests\OwnerRegisterRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OwnerAuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('owner.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('owner')->attempt($credentials)) {
+            return redirect()->intended('/owner/dashboard');
+        }
+
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('owner.register');
+    }
+
+    public function register(OwnerRegisterRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $owner = Owner::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+            'telephone' => $validatedData['telephone'],
+        ]);
+        $smsApi = new SmsApiController();
+        $message = "Dear " . $validatedData['name'] . " , your registration is successful.  Login to your account to create and manage your cooperatives";
+        $smsApi->sendSms($owner->telephone, $message);
+
+        Auth::guard('owner')->login($owner);
+
+        return redirect('/owner/dashboard');
+    }
+
+    public function logout()
+    {
+        Auth::guard('owner')->logout();
+        return redirect('/owner/login');
+    }
 }
