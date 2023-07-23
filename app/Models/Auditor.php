044d67da2ec5534010e89f2d8daef43c68521861
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 10:41:45 +0200

    app/Models/Auditor: remode;l

--- app/Models/Auditor.php
+++ app/Models/Auditor.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Auditor extends Model
+class Auditor extends Authenticatable
 {
     use HasFactory;
+    protected $fillable = ['names', 'telephone', 'email', 'password'];
+
 }
