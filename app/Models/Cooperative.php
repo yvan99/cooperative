2b008b0013e6d792c3da1d9557a13fd68f40c494
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 10:40:23 +0200

    app/Models/Cooperative: remodel

--- app/Models/Cooperative.php
+++ app/Models/Cooperative.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cooperative extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name', 'description', 'address', 'certificate', 'category_id', 'members', 'status'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function owners()
+    {
+        return $this->belongsToMany(Owner::class, 'cooperative_owners', 'cooperative_id', 'owner_id')
+            ->withTimestamps();
+    }
 }
