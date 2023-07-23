a372592067be047045e358766025fd0494dd7726
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:52:21 +0200

    database/seeders/StaffSeeder:

--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Staff;
 use Illuminate\Database\Seeder;
 
 class StaffSeeder extends Seeder
@@ -13,6 +14,6 @@ class StaffSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Staff::factory()->count(10)->create();
     }
 }
