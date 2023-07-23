e33de84f1e6d3721db894ea3a4b7a85bcba52dfd
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 10:39:43 +0200

    app/Models/Category:

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    protected $fillable = ['name'];
+    
+    public function cooperatives()
+    {
+        return $this->hasMany(Cooperative::class);
+    }
 }
