f017853fb5e97291929484c6424084091706f1c7
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 14:57:14 +0200

    app/Http/Controllers/S:

--- app/Http/Controllers/SmsApiController.php
+++ app/Http/Controllers/SmsApiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 
-class SmsController extends Controller
+class SmsApiController extends Controller
 {
 
     public function sendSms($receiver, $message)
--- app/Http/Controllers/StaffAuthController.php
+++ app/Http/Controllers/StaffAuthController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Staff;
+use App\Http\Requests\StaffRegisterRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaffAuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('staff.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('staff')->attempt($credentials)) {
+            return redirect()->intended('/staff/dashboard');
+        }
+
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('staff.register');
+    }
+
+    public function register(StaffRegisterRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $staff = Staff::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+            'telephone' => $validatedData['telephone'],
+        ]);
+
+        $smsApi = new SmsApiController();
+        $message = "Dear " . $validatedData['name'] . " , your staff account registration is successful.";
+        $smsApi->sendSms($staff->telephone, $message);
+
+        Auth::guard('staff')->login($staff);
+
+        return redirect('/staff/dashboard');
+    }
+
+    public function logout()
+    {
+        Auth::guard('staff')->logout();
+        return redirect('/staff/login');
+    }
 }
