63165cfe01e21ef1f7cf61ca9387425f558ca541
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:48:41 +0200

    database/factories/TransactionFactory:

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -2,19 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Models\Cooperative;
+use App\Models\FinanceCategory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TransactionFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Transaction::class;
+
     public function definition()
     {
         return [
-            //
+            'code' => $this->faker->unique()->numberBetween(1000, 9999),
+            'finance_category_id' => function () {
+                return FinanceCategory::factory()->create()->id;
+            },
+            'cooperative_id' => function () {
+                return Cooperative::factory()->create()->id;
+            },
+            'year' => $this->faker->year,
+            'month' => $this->faker->monthName,
+            'date' => $this->faker->date,
+            'amount' => $this->faker->randomFloat(2, 10, 1000),
+            'status' => 'pending',
+            'description' => $this->faker->paragraph,
+            'flag' => $this->faker->boolean,
         ];
     }
 }
