90d7f0d96fb534ade4ff82a405c825d2be696ae3
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 14:52:50 +0200

    app/Http/Requests/OwnerRegisterRequest:

--- app/Http/Requests/OwnerRegisterRequest.php
+++ app/Http/Requests/OwnerRegisterRequest.php
@@ -6,25 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class OwnerRegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|min:8|confirmed',
+            'telephone' => 'required|string|max:15',
         ];
     }
 }
