b30ac9029e46fd8492b934a9ef18cbd9a5ebf3df
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 10:42:21 +0200

    app/Models/Owner: remodel

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -3,9 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Owner extends Model
+class Owner extends Authenticatable
 {
     use HasFactory;
+    protected $fillable = ['names', 'telephone', 'email', 'password', 'status'];
+
+    public function cooperatives()
+    {
+        return $this->belongsToMany(Cooperative::class, 'cooperative_owners', 'owner_id', 'cooperative_id')
+            ->withTimestamps();
+    }
 }
