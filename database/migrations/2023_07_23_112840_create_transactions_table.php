4cc313803194ffb63d9d7e56a994425b442bda78
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:30:46 +0200

    database/migrations/2023_07_23_112840_create_transactions_table:

--- database/migrations/2023_07_23_112840_create_transactions_table.php
+++ database/migrations/2023_07_23_112840_create_transactions_table.php
@@ -15,6 +15,18 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->unsignedBigInteger('finance_category_id');
+            $table->foreign('finance_category_id')->references('id')->on('finance_categories');
+            $table->unsignedBigInteger('cooperative_id');
+            $table->foreign('cooperative_id')->references('id')->on('cooperatives');
+            $table->year('year');
+            $table->string('month');
+            $table->date('date');
+            $table->double('amount');
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->text('description')->nullable();
+            $table->boolean('flag')->default(false);
             $table->timestamps();
         });
     }
