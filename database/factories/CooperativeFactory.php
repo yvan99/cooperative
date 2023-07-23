3fc8babf9e34457a23fd5d1d377c66bf58364283
yvan99 <ishimweyvan90@gmail.com>
2023-07-23 13:44:52 +0200

    database/factories/CooperativeFactory:

--- database/factories/CooperativeFactory.php
+++ database/factories/CooperativeFactory.php
@@ -2,19 +2,25 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CooperativeFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Cooperative::class;
+
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->company,
+            'description' => $this->faker->paragraph,
+            'address' => $this->faker->address,
+            'certificate' => $this->faker->word . '.pdf',
+            'category_id' => function () {
+                return Category::factory()->create()->id;
+            },
+            'members' => $this->faker->randomNumber(),
+            'status' => 'pending',
         ];
     }
 }
