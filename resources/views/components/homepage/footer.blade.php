9a425b56d9801d80e8c9f8f4d95a1228e76b6afa
yvan99 <ishimweyvan90@gmail.com>
2023-08-06 23:03:34 +0200

    resources/views/components/homepage/footer.blade:

--- resources/views/components/homepage/footer.blade.php
+++ resources/views/components/homepage/footer.blade.php
@@ -1,10 +1,9 @@
-
 <section class="footer-area bg_cover" style="background-image: url(homepage/images/hero-bg.jpg);">
     <div class="container">
         <div class="row">
 
             <div class="col-lg-12">
-                <span class="copyright"> © {{env("APP_NAME")}} Developed by {{env("PROJECT_OWNER")}}</span>
+                <span class="copyright"> © {{ env('APP_NAME') }} Developed by {{ env('PROJECT_OWNER') }}</span>
             </div>
         </div>
     </div>
