ea53b5a1e210f54c03afd3f7579db124bd3bcb65
yvan99 <ishimweyvan90@gmail.com>
2023-08-06 22:57:43 +0200

    resources/views/components/homepage/js.blade: create

---
+++ resources/views/components/homepage/js.blade.php
@@ -0,0 +1,43 @@
+
+    <!--====== FOOTER PART ENDS ======-->
+
+    <!--====== BACK TO TOP START ======-->
+
+    <a class="back-to-top">
+        <i class="fal fa-angle-up"></i>
+    </a>
+
+    <!--====== BACK TO TOP ENDS ======-->
+
+
+
+
+
+
+
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+
+    <!--====== scrolling nav js ======-->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+
+<!-- Mirrored from gfxpartner.com/Corba/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 06 Aug 2023 20:30:44 GMT -->
+</html>
