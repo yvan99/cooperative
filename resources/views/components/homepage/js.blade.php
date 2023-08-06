cf64e3134c26339dfb14305169f35b8fe4791217
yvan99 <ishimweyvan90@gmail.com>
2023-08-06 22:58:16 +0200

    resources/views/components/homepage/js.blade:

--- resources/views/components/homepage/js.blade.php
+++ resources/views/components/homepage/js.blade.php
@@ -1,43 +1,16 @@
 
-    <!--====== FOOTER PART ENDS ======-->
-
-    <!--====== BACK TO TOP START ======-->
-
     <a class="back-to-top">
         <i class="fal fa-angle-up"></i>
     </a>
-
-    <!--====== BACK TO TOP ENDS ======-->
-
-
-
-
-
-
-
-    <!--====== jquery js ======-->
-    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
-    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
-
-    <!--====== Bootstrap js ======-->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-
-    <!--====== scrolling nav js ======-->
-    <script src="assets/js/jquery.easing.min.js"></script>
-    <script src="assets/js/scrolling-nav.js"></script>
-
-    <!--====== Slick js ======-->
-    <script src="assets/js/slick.min.js"></script>
-
-    <!--====== Magnific Popup js ======-->
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-
-    <!--====== Main js ======-->
-    <script src="assets/js/main.js"></script>
+    <script src="homepage/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="homepage/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="homepage/js/bootstrap.min.js"></script>
+    <script src="homepage/js/popper.min.js"></script>
+    <script src="homepage/js/jquery.easing.min.js"></script>
+    <script src="homepage/js/scrolling-nav.js"></script>
+    <script src="homepage/js/slick.min.js"></script>
+    <script src="homepage/js/jquery.magnific-popup.min.js"></script>
+    <script src="homepage/js/main.js"></script>
 
 </body>
-
-
-<!-- Mirrored from gfxpartner.com/Corba/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 06 Aug 2023 20:30:44 GMT -->
 </html>
