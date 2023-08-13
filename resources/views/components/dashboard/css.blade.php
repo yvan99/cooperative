e5e1010feb75f0de155bc40d4d5778ba091d8aff
yvan99 <ishimweyvan90@gmail.com>
2023-08-13 11:33:21 +0200

    resources/views/components/dashboard/css.blade:

--- resources/views/components/dashboard/css.blade.php
+++ resources/views/components/dashboard/css.blade.php
@@ -1,67 +1,39 @@
 <!DOCTYPE html>
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
 <head>
-        <title>Metronic - The World's #1 Selling Bootstrap Admin Template by Keenthemes</title>
-        <meta charset="utf-8"/>
-        <meta name="description" content="
-            The most advanced Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo, 
-            Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions. 
-            Grab your copy now and get life-time updates for free.
-        "/>
-        <meta name="keywords" content="
-            metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, 
-            Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates, 
-            free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, 
-            bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon
-        "/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>      
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="
-            Metronic - Bootstrap Admin Template, HTML, VueJS, React, Angular. Laravel, Asp.Net Core, Ruby on Rails, 
-            Spring Boot, Blazor, Django, Express.js, Node.js, Flask Admin Dashboard Theme & Template
-        " />
-        <meta property="og:url" content="https://keenthemes.com/metronic"/>
-        <meta property="og:site_name" content="Keenthemes | Metronic" />
-        <link rel="canonical" href="https://preview.keenthemes.com/metronic8"/>
-        <link rel="shortcut icon" href="../../../assets/media/logos/favicon.ico"/>
+    <title>{{ env('APP_NAME') }}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="../../../assets/media/logos/favicon.ico" />
 
-        <!--begin::Fonts(mandatory for all pages)-->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700"/>        <!--end::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
+    <link href="../../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
 
-        
-        
-                    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
-                            <link href="../../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css"/>
-                            <link href="../../../assets/css/style.bundle.css" rel="stylesheet" type="text/css"/>
-                        <!--end::Global Stylesheets Bundle-->
-        
+</head>
 
-    </head>
-    <!--end::Head-->
+<body id="kt_body" class="app-blank">
 
-    <!--begin::Body-->
-    <body  id="kt_body"  class="app-blank" >
-        <!--begin::Theme mode setup on page load-->
-<script>
-	var defaultThemeMode = "light";
-	var themeMode;
+    <script>
+        var defaultThemeMode = "light";
+        var themeMode;
 
-	if ( document.documentElement ) {
-		if ( document.documentElement.hasAttribute("data-bs-theme-mode")) {
-			themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
-		} else {
-			if ( localStorage.getItem("data-bs-theme") !== null ) {
-				themeMode = localStorage.getItem("data-bs-theme");
-			} else {
-				themeMode = defaultThemeMode;
-			}			
-		}
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+            } else {
+                if (localStorage.getItem("data-bs-theme") !== null) {
+                    themeMode = localStorage.getItem("data-bs-theme");
+                } else {
+                    themeMode = defaultThemeMode;
+                }
+            }
 
-		if (themeMode === "system") {
-			themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-		}
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
 
-		document.documentElement.setAttribute("data-bs-theme", themeMode);
-	}            
-</script>
+            document.documentElement.setAttribute("data-bs-theme", themeMode);
+        }
+    </script>
