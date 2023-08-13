c05d69ff99863e67e5b3a51e434e5c9b684b8b1d
yvan99 <ishimweyvan90@gmail.com>
2023-08-13 20:00:45 +0200

    resources/views/components/dashboard/css.blade:

--- resources/views/components/dashboard/css.blade.php
+++ resources/views/components/dashboard/css.blade.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <link href="../../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
     <link href="../../../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../../assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css"/>
+    <link href="../../../assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css"/>
 
 </head>
 
