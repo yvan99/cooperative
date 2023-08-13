f0ef213b52d00acfd35d67c6aa04285056a53544
yvan99 <ishimweyvan90@gmail.com>
2023-08-13 11:55:52 +0200

    resources/views/owner/login.blade:

--- resources/views/owner/login.blade.php
+++ resources/views/owner/login.blade.php
@@ -1,221 +1,102 @@
 @include('components.dashboard.css')
 
-<div class="d-flex flex-column flex-root" id="kt_app_root">
+<div class="d-flex flex-column flex-root" style="overflow: hidden" id="kt_app_root">
     <div class="d-flex flex-column flex-lg-row flex-column-fluid">
-        <!--begin::Body-->
+
         <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
-            <!--begin::Form-->
+
             <div class="d-flex flex-center flex-column flex-lg-row-fluid">
-                <!--begin::Wrapper-->
+
                 <div class="w-lg-500px p-10">
 
-                    <!--begin::Form-->
-                    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form"
-                        data-kt-redirect-url="/metronic8/demo1/../demo1/index.html" action="#">
-                        <!--begin::Heading-->
+
+                    <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" action="#">
+
                         <div class="text-left mb-11">
-                            <!--begin::Title-->
+
                             <h1 class="text-dark fw-bolder mb-3">
                                 Connect With Your Account
                             </h1>
-                            <!--end::Title-->
 
-                            <!--begin::Subtitle-->
                             <div class="text-gray-500 fw-semibold fs-6">
                                 Sign in to your cooperative account
                             </div>
-                            <!--end::Subtitle--->
+
                         </div>
-                        <!--begin::Heading-->
 
-                       
-
-                        <!--begin::Separator-->
-                        <div class="separator separator-content my-14">
-                            <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-                        </div>
-                        <!--end::Separator-->
-
-                        <!--begin::Input group--->
                         <div class="fv-row mb-8">
-                            <!--begin::Email-->
+
                             <input type="text" placeholder="Email" name="email" autocomplete="off"
                                 class="form-control bg-transparent" />
-                            <!--end::Email-->
+
                         </div>
 
-                        <!--end::Input group--->
+
                         <div class="fv-row mb-3">
-                            <!--begin::Password-->
+
                             <input type="password" placeholder="Password" name="password" autocomplete="off"
                                 class="form-control bg-transparent" />
-                            <!--end::Password-->
-                        </div>
-                        <!--end::Input group--->
 
-                        <!--begin::Wrapper-->
+                        </div>
+
                         <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                             <div></div>
 
                             <!--begin::Link-->
-                            <a href="reset-password.html" class="link-primary">
+                            {{-- <a href="reset-password.html" class="link-primary">
                                 Forgot Password ?
-                            </a>
+                            </a> --}}
                             <!--end::Link-->
                         </div>
-                        <!--end::Wrapper-->
 
-                        <!--begin::Submit button-->
                         <div class="d-grid mb-10">
                             <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
 
-                                <!--begin::Indicator label-->
+
                                 <span class="indicator-label">
                                     Sign In</span>
-                                <!--end::Indicator label-->
 
-                                <!--begin::Indicator progress-->
                                 <span class="indicator-progress">
                                     Please wait... <span
                                         class="spinner-border spinner-border-sm align-middle ms-2"></span>
                                 </span>
-                                <!--end::Indicator progress--> </button>
+                            </button>
                         </div>
-                        <!--end::Submit button-->
 
-                        <!--begin::Sign up-->
-                        <div class="text-gray-500 text-center fw-semibold fs-6">
+                        <div class="text-gray-500 text-left fw-semibold fs-6">
                             Not a Member yet?
 
-                            <a href="sign-up.html" class="link-primary">
-                                Sign up
+                            <a href="/owner/signup" class="link-primary">
+                                Create an account
                             </a>
                         </div>
-                        <!--end::Sign up-->
+
                     </form>
-                    <!--end::Form-->
+
                 </div>
-                <!--end::Wrapper-->
+
             </div>
-            <!--end::Form-->
 
-            <!--begin::Footer-->
-            <div class="w-lg-500px d-flex flex-stack px-10 mx-auto">
-                <!--begin::Languages-->
-                <div class="me-10">
-                    <!--begin::Toggle-->
-                    <button class="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base"
-                        data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start"
-                        data-kt-menu-offset="0px, 0px">
-                        <img data-kt-element="current-lang-flag" class="w-20px h-20px rounded me-3"
-                            src="../../../assets/media/flags/united-states.svg" alt="" />
-
-                        <span data-kt-element="current-lang-name" class="me-1">English</span>
-
-                        <i class="ki-duotone ki-down fs-5 text-muted rotate-180 m-0"></i> </button>
-                    <!--end::Toggle-->
-
-                    <!--begin::Menu-->
-                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7"
-                        data-kt-menu="true" id="kt_auth_lang_menu">
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="English">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="../../../assets/media/flags/united-states.svg" alt="" />
-                                </span>
-                                <span data-kt-element="lang-name">English</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="Spanish">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="../../../assets/media/flags/spain.svg" alt="" />
-                                </span>
-                                <span data-kt-element="lang-name">Spanish</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="German">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="../../../assets/media/flags/germany.svg" alt="" />
-                                </span>
-                                <span data-kt-element="lang-name">German</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="Japanese">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="../../../assets/media/flags/japan.svg" alt="" />
-                                </span>
-                                <span data-kt-element="lang-name">Japanese</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="French">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="../../../assets/media/flags/france.svg" alt="" />
-                                </span>
-                                <span data-kt-element="lang-name">French</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                    </div>
-                    <!--end::Menu-->
-                </div>
-                <!--end::Languages-->
-
-                <!--begin::Links-->
-                <div class="d-flex fw-semibold text-primary fs-base gap-5">
-                    <a href="../../../pages/team.html" target="_blank">Terms</a>
-
-                    <a href="../../../pages/pricing/column.html" target="_blank">Plans</a>
-
-                    <a href="../../../pages/contact.html" target="_blank">Contact Us</a>
-                </div>
-                <!--end::Links-->
-            </div>
-            <!--end::Footer-->
         </div>
-        <!--end::Body-->
 
-        <!--begin::Aside-->
+
+
         <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
             style="background-image: url(../../../assets/media/misc/auth-bg.png)">
-            <!--begin::Content-->
-            <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
-                <!--begin::Logo-->
-                <a href="../../../index.html" class="mb-0 mb-lg-12">
-                    <img alt="Logo" src="../../../assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
-                </a>
-                <!--end::Logo-->
 
-                <!--begin::Image-->
+            <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
+
+
                 <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
                     src="../../../assets/media/misc/auth-screens.png" alt="" />
-                <!--end::Image-->
 
-                <!--begin::Title-->
                 <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
                     Fast, Efficient and Productive
                 </h1>
                 <!--end::Title-->
 
                 <!--begin::Text-->
-                <div class="d-none d-lg-block text-white fs-base text-center">
+                {{-- <div class="d-none d-lg-block text-white fs-base text-center">
                     In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the
                         blogger</a>
 
@@ -223,7 +104,7 @@
 
                     <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>
                     and their <br /> work following this is a transcript of the interview.
-                </div>
+                </div> --}}
                 <!--end::Text-->
             </div>
             <!--end::Content-->
