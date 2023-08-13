46b16be05c717697dbfefa5af389a6e1e1f3491c
yvan99 <ishimweyvan90@gmail.com>
2023-08-13 11:34:26 +0200

    resources/views/components/dashboard/js.blade:

---
+++ resources/views/components/dashboard/js.blade.php
@@ -0,0 +1,6 @@
+<script src="../../../assets/plugins/global/plugins.bundle.js"></script>
+<script src="../../../assets/js/scripts.bundle.js"></script>
+<script src="../../../assets/js/custom/authentication/sign-in/general.js"></script>
+
+</body>
+</html>
