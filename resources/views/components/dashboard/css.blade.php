318ab18d6c8ddcd5c18981d1acfcb8b474778de5
yvan99 <ishimweyvan90@gmail.com>
2023-08-13 20:25:39 +0200

    remove footer

--- resources/views/components/dashboard/css.blade.php
+++ resources/views/components/dashboard/css.blade.php
@@ -8,14 +8,17 @@
     <link rel="shortcut icon" href="../../../assets/media/logos/favicon.ico" />
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <link href="../../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="../../../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+
     <link href="../../../assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css"/>
     <link href="../../../assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css"/>
 
+
+    <link href="../../../assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="../../../assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+
 </head>
 
-<body id="kt_body" class="app-blank">
+<body id="kt_app_body" data-kt-app-header-fixed-mobile="true" data-kt-app-toolbar-enabled="true"  class="app-default">
 
     <script>
         var defaultThemeMode = "light";
