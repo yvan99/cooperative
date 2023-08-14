14d9d6f5d3abac773cd46295b4c33f0ac78748e8
yvan99 <ishimweyvan90@gmail.com>
2023-08-15 00:07:07 +0200

    resources/views:

---
+++ resources/views/owner/components/navbar.blade.php
@@ -0,0 +1,16 @@
+<div class="nav-scroller bg-body shadow-sm">
+    <div class="container-fluid">
+        <nav class="nav nav-underline" aria-label="Secondary navigation">
+            <ul class="iq-nav-menu list-unstyled flex-row">
+                <li class="nav-item active"><a class="nav-link " href="../dashboard/index-dual-compact.html">Dashboard</a>
+                </li>
+                <li class="nav-item"><a class="nav-link " href="../dashboard/analytics-dashboard.html">Analytics</a></li>
+                <li class="nav-item"><a class="nav-link " href="../dashboard/ecommerce-dashboard.html">E-Commerce</a>
+                </li>
+                <li class="nav-item"><a class="nav-link " href="../dashboard/crypto-dashboard.html">Crypto</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+</div> <!--Nav End-->
