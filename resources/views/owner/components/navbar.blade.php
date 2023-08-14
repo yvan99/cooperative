b8334106167e4b724493e8fb6dbe163eab8f48ac
yvan99 <ishimweyvan90@gmail.com>
2023-08-15 00:29:58 +0200

    resources/views/owner/components/navbar.blade:

--- resources/views/owner/components/navbar.blade.php
+++ resources/views/owner/components/navbar.blade.php
@@ -4,10 +4,17 @@
             <ul class="iq-nav-menu list-unstyled flex-row">
                 <li class="nav-item active"><a class="nav-link " href="../dashboard/index-dual-compact.html">Dashboard</a>
                 </li>
-                <li class="nav-item"><a class="nav-link " href="../dashboard/analytics-dashboard.html">Analytics</a></li>
-                <li class="nav-item"><a class="nav-link " href="../dashboard/ecommerce-dashboard.html">E-Commerce</a>
+                <li class="nav-item"><a class="nav-link " href="../dashboard/analytics-dashboard.html">Cooperatives</a>
                 </li>
-                <li class="nav-item"><a class="nav-link " href="../dashboard/crypto-dashboard.html">Crypto</a>
+                <li class="nav-item"><a class="nav-link " href="../dashboard/ecommerce-dashboard.html">Accounts</a>
+                </li>
+                <li class="nav-item"><a class="nav-link " href="../dashboard/crypto-dashboard.html">Transactions</a>
+                </li>
+
+                <li class="nav-item"><a class="nav-link " href="../dashboard/crypto-dashboard.html">Analytics</a>
+                </li>
+
+                <li class="nav-item"><a class="nav-link " href="../dashboard/crypto-dashboard.html">Financial Audit</a>
                 </li>
             </ul>
         </nav>
