d735f9514fb39164122d1387f50f093286228df7
yvan99 <ishimweyvan90@gmail.com>
2023-08-15 18:03:49 +0200

    change account model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
-    protected $fillable = ['code', 'name', 'cooperative_id', 'description', 'status'];
+    protected $fillable = ['code', 'name', 'cooperative_id', 'description', 'status','amount'];
     
     public function cooperative()
     {
