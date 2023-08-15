8d56e9c07f848952da649fb434eab8ad8ddbfa83
yvan99 <ishimweyvan90@gmail.com>
2023-08-15 16:25:00 +0200

    resources/views/owner/components/breadcrumb.blade:

--- resources/views/owner/components/breadcrumb.blade.php
+++ resources/views/owner/components/breadcrumb.blade.php
@@ -3,7 +3,32 @@
         <h3>Dashboard</h3>
         <p class="mb-0">Financial Dashboard</p>
     </div>
-    <div class="d-flex justify-content-between align-items-center rounded flex-wrap gap-3">
-        <button type="button" class="btn btn-primary">Urujana Cooperative</button>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#setDefaultCooperativeModal">
+        Default cooperative
+    </button>
+</div>
+
+<div class="modal fade" id="setDefaultCooperativeModal" tabindex="-1" role="dialog" aria-labelledby="setDefaultCooperativeModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="setDefaultCooperativeModalLabel">Select Default Cooperative</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    @foreach ($cooperatives as $cooperative)
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="defaultCooperative" value="{{ $cooperative->id }}" id="defaultCooperative{{ $cooperative->id }}">
+                            <label class="form-check-label" for="defaultCooperative{{ $cooperative->id }}">
+                                {{ $cooperative->name }}
+                            </label>
+                        </div>
+                    @endforeach
+                    <button type="button" class="btn btn-primary" id="saveDefaultCooperative">Save</button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
+
