064db2543f497f34515b99ddbea17b3bd8e891c5
yvan99 <ishimweyvan90@gmail.com>
2023-08-15 14:08:53 +0200

    resources/views/cooperatives/index.blade:

--- resources/views/cooperatives/index.blade.php
+++ resources/views/cooperatives/index.blade.php
@@ -5,28 +5,23 @@
     <main class="main-content">
         @include('components.dashboard.topnav')
         @include('owner.components.navbar')
-
         <div class="container-fluid content-inner pb-0" id="page_layout">
+
             @include('owner.components.breadcrumb')
 
             <div class="row">
-
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header d-flex justify-content-between">
                             <div class="header-title">
                                 <h4 class="card-title">Cell Admins</h4>
-
                             </div>
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                 data-bs-target="#createCategoryModal">
                                 Register Cooperative
                             </button>
-
-
                         </div>
                         <div class="card-body">
-
                             @include('components.dashboard.alert')
                             <div class="table-responsive border rounded">
 
@@ -35,15 +30,38 @@
                                         <tr>
                                             <th>Name</th>
                                             <th>Description</th>
+                                            <th>Certificate</th>
+                                            <th>Category</th>
+                                            <th>Currency</th>
+                                            <th>Address</th>
+                                            <th>Status</th>
+                                            <th>Members</th>
                                             <th>Created At</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-
+                                        @foreach ($cooperatives as $cooperative)
+                                            <tr>
+                                                <td>{{ $cooperative->name }}</td>
+                                                <td>{{ $cooperative->description }}</td>
+                                                <td>
+                                                    @if ($cooperative->certificate)
+                                                        <a href="{{ Storage::url($cooperative->certificate) }}"
+                                                            target="_blank">Certificate</a>
+                                                    @else
+                                                        No Certificate
+                                                    @endif
+                                                </td>
+                                                <td>{{ $cooperative->category->name }}</td>
+                                                <td>{{ $cooperative->currency }}</td>
+                                                <td>{{ $cooperative->address }}</td>
+                                                <td>{{ $cooperative->status }}</td>
+                                                <td>{{ $cooperative->members }}</td>
+                                                <td>{{ $cooperative->created_at }}</td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
-
-
 
                                 <div class="modal fade" id="createCategoryModal" tabindex="-1" role="dialog"
                                     aria-labelledby="createCategoryModalLabel" aria-hidden="true">
@@ -67,14 +85,15 @@
                                                     </div>
 
                                                     <div class="form-group">
-                                                        <label class="mb-3" for="certificate">Certificate (PDF)</label>
+                                                        <label class="mb-3" for="certificate">Certificate
+                                                            (PDF)</label>
                                                         <input type="file" class="form-control" id="certificate"
                                                             name="certificate" accept=".pdf" required>
                                                     </div>
                                                     <div class="form-group">
                                                         <label class="mb-3" for="category_id">Category</label>
-                                                        <select class="form-control form-select" id="category_id" name="category_id"
-                                                            required>
+                                                        <select class="form-control form-select" id="category_id"
+                                                            name="category_id" required>
                                                             @foreach ($categories as $category)
                                                                 <option value="{{ $category->id }}">
                                                                     {{ $category->name }}</option>
@@ -109,17 +128,10 @@
                                         </div>
                                     </div>
                                 </div>
-
-
-
-
-
-
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
         @include('components.dashboard.dashfooter')
