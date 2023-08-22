210001d468921ee6696f50b732b1fe86bc4707a7
yvan99 <ishimweyvan90@gmail.com>
2023-08-22 23:58:22 +0200

    resources/views/staff/components/navbar.blade:

--- resources/views/staff/components/navbar.blade.php
+++ resources/views/staff/components/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <nav class="nav nav-underline" aria-label="Secondary navigation">
             <ul class="iq-nav-menu list-unstyled flex-row">
-                <li class="nav-item {{ Request::is('staff/dashboard') ? 'active' : '' }}"><a class="nav-link " href="/staff/dashboard">Dashboard</a>
+                <li class="nav-item {{ Request::is('staff/dashboard') ? 'active' : '' }}"><a class="nav-link " href="/staff/dashboard">Manage Cooperatives</a>
                 </li>
                 <li class="nav-item {{ Request::is('staff/auditors') ? 'active' : '' }}"><a class="nav-link " href="/staff/auditors">Manage Auditors</a>
                 </li>
