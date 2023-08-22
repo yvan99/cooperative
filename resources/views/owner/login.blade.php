088bf8edf5789c586032298e27cb9e17b8c8998f
yvan99 <ishimweyvan90@gmail.com>
2023-08-22 21:27:51 +0200

    resources/views/owner/login.blade:

--- resources/views/owner/login.blade.php
+++ resources/views/owner/login.blade.php
@@ -82,7 +82,7 @@
 
 
 
-        <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
+        {{-- <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
             style="background-image: url(../../../assets/media/misc/auth-bg.png)">
 
             <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
@@ -94,24 +94,10 @@
                 <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
                     Fast, Efficient and Productive
                 </h1>
-                <!--end::Title-->
-
-                <!--begin::Text-->
-                {{-- <div class="d-none d-lg-block text-white fs-base text-center">
-                    In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the
-                        blogger</a>
-
-                    introduces a person they’ve interviewed <br /> and provides some background information about
-
-                    <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>
-                    and their <br /> work following this is a transcript of the interview.
-                </div> --}}
-                <!--end::Text-->
+ 
             </div>
-            <!--end::Content-->
-        </div>
-        <!--end::Aside-->
+        </div> --}}
+
     </div>
-    <!--end::Authentication - Sign-in-->
 </div>
 @include('components.dashboard.js')
