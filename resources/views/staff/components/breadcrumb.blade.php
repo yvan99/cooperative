4b97fd27a20aa2d3bcebe08ff8cd4379be662b08
yvan99 <ishimweyvan90@gmail.com>
2023-08-22 22:54:37 +0200

    resources/views/staff:

---
+++ resources/views/staff/components/breadcrumb.blade.php
@@ -0,0 +1,76 @@
+<div class="d-flex justify-content-between align-items-center flex-wrap mb-5 gap-3">
+    <div class="d-flex flex-column">
+        <h3>Dashboard</h3>
+        <p class="mb-0">Financial Dashboard</p>
+    </div>
+    <button type="button" class="btn btn-primary default-cooperative-button" data-bs-toggle="modal" data-bs-target="#setDefaultCooperativeModal">
+        Set Default Cooperative
+    </button>
+    
+</div>
+
+<div class="modal fade" id="setDefaultCooperativeModal" tabindex="-1" role="dialog"
+    aria-labelledby="setDefaultCooperativeModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="setDefaultCooperativeModalLabel">Select Default Cooperative</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    @foreach ($ownerCooperatives as $cooperative)
+                    <div class="form-check">
+                        <input class="form-check-input form-check-input-lg" type="radio" name="defaultCooperative"
+                               value="{{ $cooperative->id }}" id="defaultCooperative{{ $cooperative->id }}">
+                        <label class="form-check-label fs-5 mb-3" for="defaultCooperative{{ $cooperative->id }}">
+                            {{ $cooperative->name }}
+                        </label>
+                    </div>
+                @endforeach
+                
+
+
+                    <button type="button" class="btn btn-primary mt-3" id="saveDefaultCooperative">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const storedDefaultCooperativeId = localStorage.getItem('defaultCooperativeId');
+
+        if (storedDefaultCooperativeId) {
+            const radioInput = document.getElementById(`defaultCooperative${storedDefaultCooperativeId}`);
+            if (radioInput) {
+                radioInput.checked = true;
+            }
+        }
+
+        const defaultCooperativeButton = document.querySelector('.default-cooperative-button');
+        const storedDefaultCooperativeName = localStorage.getItem('defaultCooperativeName');
+
+        if (storedDefaultCooperativeName) {
+            defaultCooperativeButton.textContent = storedDefaultCooperativeName;
+        }
+
+        const saveButton = document.getElementById('saveDefaultCooperative');
+        saveButton.addEventListener('click', function() {
+            const selectedCooperative = document.querySelector(
+                'input[name="defaultCooperative"]:checked');
+            if (selectedCooperative) {
+                const cooperativeId = selectedCooperative.value;
+                const cooperativeName = selectedCooperative.nextElementSibling.textContent.trim();
+
+                localStorage.setItem('defaultCooperativeId', cooperativeId);
+                localStorage.setItem('defaultCooperativeName', cooperativeName);
+
+                alert('Default cooperative set.');
+                location.reload();
+            } else {
+                alert('Please select a default cooperative.');
+            }
+        });
+    });
+</script>
---
+++ resources/views/staff/dashboard.blade.php
@@ -0,0 +1,82 @@
+@include('components.dashboard.dashcss')
+
+<body class="  dual-compact">
+    <span class="screen-darken"></span>
+    <main class="main-content">
+        @include('components.dashboard.topnav')
+        @include('owner.components.navbar')
+
+        <div class="container-fluid content-inner pb-0" id="page_layout">
+            @include('owner.components.breadcrumb')
+            <div class="row">
+                <div class="col-lg-8 col-xl-8">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card card-block card-stretch card-height">
+                                <div class="flex-wrap card-header d-flex justify-content-between align-items-center">
+                                    <div class="header-title">
+                                        <h4>Sales Stastics</h4>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="me-3">
+                                            <p class="mb-0"><svg class="text-primary" width="10" height="10"
+                                                    viewBox="0 0 10 10" fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <circle cx="5" cy="5" r="5"
+                                                        fill="currentColor" />
+                                                </svg> Total Sales </p>
+                                        </div>
+                                        <div class="me-3">
+                                            <p class="mb-0"><svg class="text-secondary" width="10" height="10"
+                                                    viewBox="0 0 10 10" fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <circle cx="5" cy="5" r="5"
+                                                        fill="currentColor" />
+                                                </svg> Total Expense</p>
+                                        </div>
+                                        <div class="">
+                                            <p class="mb-0"><svg class="text-tertiray" width="10" height="10"
+                                                    viewBox="0 0 10 10" fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <circle cx="5" cy="5" r="5"
+                                                        fill="currentColor" />
+                                                </svg> Total Profit</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div id="sales-chart-02" class="sales-chart-02"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card card-block card-stretch card-height">
+                        <div class="flex-wrap card-header d-flex justify-content-between align-items-center">
+                            <div class="header-title">
+                                <h4>Sales Anylsis</h4>
+                            </div>
+                            <div class="dropdown">
+                                <a href="#" class="text-gray dropdown-toggle" id="dropdownMenuButton29"
+                                    data-bs-toggle="dropdown" aria-expanded="false">All Tasks</a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton29"
+                                    style="">
+                                    <li><a class="dropdown-item" href="#">This Week</a></li>
+                                    <li><a class="dropdown-item" href="#">This Month</a></li>
+                                    <li><a class="dropdown-item" href="#">This Year</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div id="sales-chart-04" class="sales-chart-04"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        @include('components.dashboard.dashfooter')
+    </main>
+    @include('components.dashboard.dashjs')
