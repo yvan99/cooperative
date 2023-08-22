741071a574fe3c9f53a0d302c04582fbcd2f90bf
yvan99 <ishimweyvan90@gmail.com>
2023-08-22 21:37:55 +0200

    resources/views/welcome.blade:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,94 @@
-@include('components.homepage.top')
-<section class="hero-area bg_cover mb-2 pb-5" style="background-image: url(homepage/images/hero-bg.jpg);">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="hero-content">
-                    <h1 class="title"><span>Communicate</span> with your friends and family.</h1>
-                    <ul class="nav">
-                        <li><a class="main-btn" href="#">Get started <i class="fas fa-angle-right"></i></a></li>
-                        <li><a class="main-btn main-btn-2" href="#">Learn more <i
-                                    class="fas fa-angle-right"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-            {{-- <div class="col-lg-6">
-                <div class="hero-thumb">
-                    <img src="homepage/images/hero1.png" style="width: 600px !important" alt="">
-                </div>
-            </div> --}}
-        </div>
-    </div>
-</section>
+<!DOCTYPE html>
+<html lang="en">
 
-<section id="intro" class="features-area mt-3">
-    <div class="container custom-container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="features-box">
-                    <div class="features-title">
-                        <h3 class="title">Get to know corba’s <br> advanced features.</h3>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-3">
-                            <div class="features-item mt-30">
-                                <i class="fal fa-comments"></i>
-                                <h4 class="title">Simple and reliable messaging</h4>
-                                <p>Make. Lights us. Is life all make midst a moveth forth under may Cattle moved
-                                    without seas first air beast place after.</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="features-item mt-30">
-                                <i class="fal fa-users-crown"></i>
-                                <h4 class="title">Make private groups for closed ones</h4>
-                                <p>Make. Lights us. Is life all make midst a moveth forth under may Cattle moved
-                                    without seas first air beast place after.</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-3">
-                            <div class="features-item mt-30">
-                                <i class="fal fa-user-lock"></i>
-                                <h4 class="title">Your data encrypted and 100% secure</h4>
-                                <p>Make. Lights us. Is life all make midst a moveth forth under may Cattle moved
-                                    without seas first air beast place after.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="features-play">
-                        <a class="video-popup" href="https://www.youtube.com/watch?v=MAvJ5FyM6cI"><i
-                                class="fas fa-play"></i></a>
-                    </div>
-                </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Landing Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <style>
+        .hero-section {
+            background-image: url('your-image.jpg');
+            /* Replace 'your-image.jpg' with your actual image path */
+            background-size: cover;
+            background-position: center;
+            position: relative;
+            color: #fff;
+        }
+
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            /* Adjust the opacity as needed */
+        }
+    </style>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Your Brand</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#features">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
-</section>
+    </nav>
 
-<section class="download-app-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7">
-                <div class="download-app-content">
-                    <h3 class="title">Download the app with everything for free.</h3>
-                    <ul class="nav">
-                        <li><a class="main-btn main-btn-3" href="#">Get started <i
-                                    class="fas fa-angle-right"></i></a></li>
-                        <li><a class="main-btn main-btn-2 main-btn-4" href="#">Learn more <i
-                                    class="fas fa-angle-right"></i></a></li>
-                    </ul>
+    <section class="hero-section text-center text-white py-5">
+        <div class="overlay"></div>
+        <div class="container">
+            <h1 class="display-4">Welcome to Our Website</h1>
+            <p class="lead">This is a simple landing page built with Bootstrap.</p>
+            <a href="#features" class="btn btn-primary">Learn More</a>
+        </div>
+    </section>
+
+    <section id="features" class="py-5">
+        <div class="container text-center">
+            <h2 class="display-4">Features</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Feature 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Feature 2</h3>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Feature 3</h3>
+                    <p>Duis aute irure dolor in reprehenderit in voluptate velit.</p>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="download-app-case">
-        <img src="assets/images/download-app-case.png" alt="">
-    </div>
-</section>
+    </section>
 
 
+    <footer class="text-center py-3">
+        <p>&copy; {{ date('Y') }} Powered by {{ env('APP_OWNER') }}</p>
+    </footer>
 
-<!--====== HERO PART ENDS ======-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 
-
-{{-- @include('components.homepage.footer') --}}
-@include('components.homepage.js')
+</html>
