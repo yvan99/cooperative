4d92f94841a02b08f367905e6c260eb1cd39edc5
yvan99 <ishimweyvan90@gmail.com>
2023-08-22 22:04:13 +0200

    resources/views/welcome.blade:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,10 @@
             left: 0;
             width: 100%;
             height: 100%;
-            background-color: rgba(0, 0, 0, 0.7);
+            background-color: rgba(0, 0, 0, 0.8);
+        }
+        .hero-container{
+            margin-top: 100px;
         }
     </style>
 
@@ -38,14 +41,11 @@
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#features">Features</a>
-                    </li>
+                        <a class="btn btn-sm btn-secondary" href="/auditor/login">Auditor Portal</a>
+                    </li> &nbsp;
                     <li class="nav-item">
-                        <a class="nav-link" href="#contact">Contact</a>
+                        <a class="btn btn-sm btn-dark" href="/admin/login">Admin Portal</a>
                     </li>
                 </ul>
             </div>
@@ -54,34 +54,13 @@
 
     <section class="hero-section text-center text-white py-5">
         <div class="overlay"></div>
-        <div class="container position-relative mt-5">
+        <div class="container position-relative hero-container">
             <h1 class="display-4">{{env("APP_TITLE")}}</h1>
             <p class="lead">{{env("APP_DESCRIPTION")}}</p>
             <a href="/owner/register" class="btn btn-dark">Create Account</a>
             <a href="/owner/login" class="btn btn-secondary">Login</a>
         </div>
     </section>
-
-    {{-- <section id="features" class="py-5">
-        <div class="container text-center">
-            <h2 class="display-4">Features</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <h3>Feature 1</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div class="col-md-4">
-                    <h3>Feature 2</h3>
-                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                </div>
-                <div class="col-md-4">
-                    <h3>Feature 3</h3>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit.</p>
-                </div>
-            </div>
-        </div>
-    </section> --}}
-
 
     <footer class="text-left py-3 container">
         <p>&copy; {{ date('Y') }} All rights reserved , Developed by {{ env('APP_OWNER') }}</p>
