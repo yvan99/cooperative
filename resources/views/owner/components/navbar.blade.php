522367024e91d9e33c48267ccffd4ebeb5a570ec
yvan99 <ishimweyvan90@gmail.com>
2023-08-25 22:46:57 +0200

    resources/views/owner/components/navbar.blade:

--- resources/views/owner/components/navbar.blade.php
+++ resources/views/owner/components/navbar.blade.php
@@ -21,7 +21,7 @@
                 <li class="nav-item {{ Request::is('owner/audit') ? 'active' : '' }}"><a class="nav-link " href="/owner/audit">Financial Audit</a>
                 </li>
 
-                <li class="nav-item {{ Request::is('owner/logout') ? 'active' : '' }}"><a class="nav-link " href="/owner/logout">Logout</a>
+                <li class="nav-item {{ Request::is('owner/logout') ? 'active' : '' }}"><a class="nav-link text-white bg-danger" href="/owner/logout">Logout</a>
                 </li>
             </ul>
         </nav>
