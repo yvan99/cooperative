11fe17c9d57dce808ced37de66d58cd18c0725b2
yvan99 <ishimweyvan90@gmail.com>
2023-08-26 01:02:47 +0200

    resources/views/auditor:

---
+++ resources/views/auditor/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<div class="nav-scroller bg-body shadow-sm">
+    <div class="container-fluid">
+        <nav class="nav nav-underline" aria-label="Secondary navigation">
+            <ul class="iq-nav-menu list-unstyled flex-row">
+                <li class="nav-item {{ Request::is('staff/dashboard') ? 'active' : '' }}"><a class="nav-link " href="/staff/dashboard">Manage Cooperatives</a>
+                </li>
+                <li class="nav-item {{ Request::is('staff/auditors') ? 'active' : '' }}"><a class="nav-link " href="/staff/auditors">Manage Auditors</a>
+                </li>
+                <li class="nav-item {{ Request::is('staff/logout') ? 'active' : '' }}"><a class="nav-link " href="/staff/logout">Logout</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+</div> <!--Nav End-->
