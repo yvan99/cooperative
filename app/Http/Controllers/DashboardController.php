583d2941402236cb531b7592ef9cc7f758d06733
yvan99 <ishimweyvan90@gmail.com>
2023-08-26 00:17:11 +0200

    app/Http/Controllers/DashboardController:

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,45 +1,58 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\Cooperative;
-    use App\Models\Transaction;
-    use Illuminate\Support\Facades\Auth;
-    use Kakaprodo\SystemAnalytic\AnalyticGate;
+use App\Models\Cooperative;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Kakaprodo\SystemAnalytic\AnalyticGate;
 
-    class DashboardController extends Controller {
-        public function index() {
-            $incomeVsExpence = AnalyticGate::process([ 'analytic_type' => 'income_vs_expence' ])['result'];
+class DashboardController extends Controller
+{
 
-            // Charts analytics
-            $analytics = collect([
-                'categories' => json_encode(AnalyticGate::process([ 'analytic_type' => 'categories' ])['result']),
+    public function index()
+    {
+        $incomeVsExpence = AnalyticGate::process(['analytic_type' => 'income_vs_expence'])['result'];
 
-                'transactions' => json_encode([
-                    ['name' => 'Expences Avarage', 'data' => AnalyticGate::process([ 'analytic_type' => 'expences-analytics' ])['result']],
-                    ['name' => 'Incomes Average', 'data' => AnalyticGate::process([ 'analytic_type' => 'incomes-analytics' ])['result']],
-                ]),
+        $defaultCooperativeId = Session::get('defaultCooperativeId');
 
-                'incone_vs_expences' => json_encode([
-                    [ 'name' => 'Income', 'y' => $incomeVsExpence['incomes'] ],
-                    [ 'name' => 'Expences', 'y' => $incomeVsExpence['expences'] ],
-                ]),
-            ]);
-
-            // Computed anaytics
-            $coops = Cooperative::whereHas('owners', fn($query) => $query->where('owner_id', Auth::user()->id))->count();
-            $trans = Transaction::whereHas('cooperative', fn($query) => $query->where('id', Auth::user()->id))->count();
-            $expences = Transaction::whereHas('financeCategory', fn($query) => $query->where('type', 'expense'))->avg('amount');
-            $incomes = Transaction::whereHas('financeCategory', fn($query) => $query->where('type', 'income'))->avg('amount');
-
-            $computedAnalytics = [
-                'coops' => $coops,
-                'incomes' => $incomes,
-                'expences' => $expences,
-                'transactions' => $trans,
-            ];
-
-            return view('owner.dashboard', compact('analytics', 'computedAnalytics'));
-
+        if (!$defaultCooperativeId) {
+            return redirect()->route('owner.dashboard')->with('error', 'Please set a default cooperative first.');
         }
+
+        // Charts analytics
+        $analytics = collect([
+            'categories' => json_encode(AnalyticGate::process(['analytic_type' => 'categories'])['result']),
+
+            'transactions' => json_encode([
+                ['name' => 'Expences Avarage', 'data' => AnalyticGate::process(['analytic_type' => 'expences-analytics'])['result']],
+                ['name' => 'Incomes Average', 'data' => AnalyticGate::process(['analytic_type' => 'incomes-analytics'])['result']],
+            ]),
+
+            'incone_vs_expences' => json_encode([
+                ['name' => 'Income', 'y' => $incomeVsExpence['incomes']],
+                ['name' => 'Expences', 'y' => $incomeVsExpence['expences']],
+            ]),
+        ]);
+
+        // Computed anaytics
+        $coops = Cooperative::whereHas('owners', fn ($query) => $query->where('owner_id', Auth::user()->id))->count();
+        $trans = Transaction::whereHas('cooperative', fn ($query) => $query->where('id', $defaultCooperativeId))->count();
+        $expences = Transaction::whereHas('financeCategory', fn ($query) => $query->where('type', 'expense'))
+            ->where('cooperative_id', $defaultCooperativeId)
+            ->avg('amount');
+        $incomes = Transaction::whereHas('financeCategory', fn ($query) => $query->where('type', 'income'))
+            ->where('cooperative_id', $defaultCooperativeId)
+            ->avg('amount');
+
+        $computedAnalytics = [
+            'coops' => $coops,
+            'incomes' => $incomes,
+            'expences' => $expences,
+            'transactions' => $trans,
+        ];
+
+        return view('owner.dashboard', compact('analytics', 'computedAnalytics'));
     }
+}
