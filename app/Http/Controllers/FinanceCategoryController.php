2510dc35b6f324fb6b1425b689a467c8a441309f
yvan99 <ishimweyvan90@gmail.com>
2023-08-25 23:57:36 +0200

    app/Http/Controllers/FinanceCategoryController:

--- app/Http/Controllers/FinanceCategoryController.php
+++ app/Http/Controllers/FinanceCategoryController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\FinanceCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class FinanceCategoryController extends Controller
 {
     public function index()
     {
-        $financeCategories = FinanceCategory::all();
+        $defaultCooperativeId = Session::get('defaultCooperativeId');
+        
+        if (!$defaultCooperativeId) {
+            return redirect()->route('owner.dashboard')->with('error', 'Please set a default cooperative first.');
+        }
+    
+        $financeCategories = FinanceCategory::where('cooperative_id', $defaultCooperativeId)->get();
         return view('categories.index', compact('financeCategories'));
     }
+    
 
     public function store(Request $request)
     {
