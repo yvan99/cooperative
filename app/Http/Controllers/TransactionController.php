88af822ba3d486b6c3cbe17321349513dc9c85d2
yvan99 <ishimweyvan90@gmail.com>
2023-08-26 00:04:29 +0200

    app/Http/Controllers/TransactionController: show data based on default cooperative

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -1,88 +1,103 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\Account;
-    use App\Models\Cooperative;
-    use App\Models\FinanceCategory;
-    use App\Models\Transaction;
-    use Illuminate\Http\Request;
-    use PDF;
+use App\Models\Account;
+use App\Models\Cooperative;
+use App\Models\FinanceCategory;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use PDF;
 
-    class TransactionController extends Controller {
-        public function index() {
-            $transactions = Transaction::all();
-            $financeCategories = FinanceCategory::all();
-            $cooperatives = Cooperative::all();
-            $accounts = Account::all();
+class TransactionController extends Controller
+{
 
-            return view('Transaction.index', compact('transactions', 'financeCategories', 'cooperatives', 'accounts'));
+
+    public function index()
+    {
+        $defaultCooperativeId = Session::get('defaultCooperativeId');
+
+        if (!$defaultCooperativeId) {
+            return redirect()->route('owner.dashboard')->with('error', 'Please set a default cooperative first.');
         }
 
-        public function store(Request $request) {
-            $getGeneratedCode = new AccountController;
+        $transactions = Transaction::where('cooperative_id', $defaultCooperativeId)->get();
+        $financeCategories = FinanceCategory::where('cooperative_id', $defaultCooperativeId)->get();
+        $accounts = Account::where('cooperative_id', $defaultCooperativeId)->get();
+        // $cooperatives = Cooperative::all();
 
-            $request->validate([
-                'finance_category_id' => 'required|exists:finance_categories,id',
-                'cooperative_id' => 'required|exists:cooperatives,id',
-                'amount' => 'required|numeric',
-                'account_id' => 'nullable|exists:accounts,id',
-            ]);
-
-
-            $transaction = new Transaction([
-                'code' => $getGeneratedCode->generateRandomAccountCode('TX-'),
-                'finance_category_id' => $request->finance_category_id,
-                'cooperative_id' => $request->cooperative_id,
-                'year' => now()->year,
-                'month' => now()->format('F'),
-                'date' => now()->toDateString(),
-                'amount' => $request->amount,
-                'status' => 'approved',
-                'account_id' => $request->account_id,
-                'description' => $request->description,
-            ]);
-
-            if ($transaction->financeCategory->type === 'income') {
-                $account = Account::find($request->account_id);
-
-                if ($account) {
-                    $account->amount += $transaction->amount;
-
-
-                    $account->save();
-                }
-            } elseif ($transaction->financeCategory->type === 'expense') {
-                $account = Account::find($request->account_id);
-                if ($account) {
-                    $account->amount -= $transaction->amount;
-                    $account->save();
-                }
-            }
-            $transaction->save();
-
-            return redirect()->back()->with('success', 'Transaction created successfully.');
-        }
-
-        /**
-         * Generate reports for transactions within Given Yeah and Month.
-         *
-         * Retrieves and generates reports for all transactions that occurred within
-         * the Given Yeah and Month.
-         *
-         * @param Request $request
-         * @return \Illuminate\Support\Collection
-         */
-        public function generateReports(Request $request) {
-            $year = $request->input('year');
-            $month = $request->input('month');
-
-            $transactions = Transaction::whereYear('date', $year)->whereMonth('date', $month)->get();
-            $report = [ 'year' => $year, 'month' => $month, 'transactions' => $transactions ];
-
-            view()->share('transactions', $report);
-            $pdf = PDF::loadView('transaction.report', $report);
-
-            return $pdf->download($year.'_'.$month.'_transactions'.'.pdf');
-        }
+        return view('Transaction.index', compact('transactions', 'financeCategories', 'accounts'));
     }
+
+
+
+    public function store(Request $request)
+    {
+        $getGeneratedCode = new AccountController;
+
+        $request->validate([
+            'finance_category_id' => 'required|exists:finance_categories,id',
+            'cooperative_id' => 'required|exists:cooperatives,id',
+            'amount' => 'required|numeric',
+            'account_id' => 'nullable|exists:accounts,id',
+        ]);
+
+
+        $transaction = new Transaction([
+            'code' => $getGeneratedCode->generateRandomAccountCode('TX-'),
+            'finance_category_id' => $request->finance_category_id,
+            'cooperative_id' => $request->cooperative_id,
+            'year' => now()->year,
+            'month' => now()->format('F'),
+            'date' => now()->toDateString(),
+            'amount' => $request->amount,
+            'status' => 'approved',
+            'account_id' => $request->account_id,
+            'description' => $request->description,
+        ]);
+
+        if ($transaction->financeCategory->type === 'income') {
+            $account = Account::find($request->account_id);
+
+            if ($account) {
+                $account->amount += $transaction->amount;
+
+
+                $account->save();
+            }
+        } elseif ($transaction->financeCategory->type === 'expense') {
+            $account = Account::find($request->account_id);
+            if ($account) {
+                $account->amount -= $transaction->amount;
+                $account->save();
+            }
+        }
+        $transaction->save();
+
+        return redirect()->back()->with('success', 'Transaction created successfully.');
+    }
+
+    /**
+     * Generate reports for transactions within Given Yeah and Month.
+     *
+     * Retrieves and generates reports for all transactions that occurred within
+     * the Given Yeah and Month.
+     *
+     * @param Request $request
+     * @return \Illuminate\Support\Collection
+     */
+    public function generateReports(Request $request)
+    {
+        $year = $request->input('year');
+        $month = $request->input('month');
+
+        $transactions = Transaction::whereYear('date', $year)->whereMonth('date', $month)->get();
+        $report = ['year' => $year, 'month' => $month, 'transactions' => $transactions];
+
+        view()->share('transactions', $report);
+        $pdf = PDF::loadView('transaction.report', $report);
+
+        return $pdf->download($year . '_' . $month . '_transactions' . '.pdf');
+    }
+}
